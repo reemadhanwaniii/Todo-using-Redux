857b115dd0a14ff581bdfe8851c4ecd7cdcce4b9
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-11-09 19:11:33 +0530

    integrate reduc devtool

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { combineReducers, createStore } from "redux";
 import  TodoReducer  from "./reducers/TodoReducer";
 
-const store = createStore(combineReducers(TodoReducer),{});
+const reduxDevtoolEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(combineReducers(TodoReducer),{},reduxDevtoolEnhancer);
 
 export default store;
