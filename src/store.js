481451045e41f019e7430b7e81b1a7272291bf17
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-11-09 19:12:51 +0530

    added comment how to install devtool

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
 import { combineReducers, createStore } from "redux";
 import  TodoReducer  from "./reducers/TodoReducer";
 
+
+/**
+ * install redux devtool chrome extension
+ * https://github.com/reduxjs/redux-devtools/tree/main/extension#installation go to this url
+ * 1.1 basic store copy from there
+ */
 const reduxDevtoolEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(combineReducers(TodoReducer),{},reduxDevtoolEnhancer);
 
